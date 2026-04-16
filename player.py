78fb93bcd586b781f37383b9ccdf9426944586c7
Alessandro-Sandiano <s346155@studenti.polito.it>
2026-04-16 06:24:53 +0200

    Modifica dei commenti

--- player.py
+++ player.py
@@ -7,19 +7,17 @@ class Player:
 
     def load_players(self, file_name):
         """
-        Load the file of players and put them in a list with their own score, including the current player.
+        Load the file of players and put them into a list with their own score, including the current player.
         :param file_name: name of the file to load.
         :return: list of Player objects.
         """
         scores = [f"{self._name} {self._score}\n"]
         with open(f"{file_name}.txt", encoding="utf-8") as file:
-            '''
-            while True:
-                scores.append(file.readline())
-                if scores[len(scores)-1]=="":
-                    scores.pop(len(scores)-1)
-                    break
-            '''
+            # while True:
+            #     scores.append(file.readline())
+            #     if scores[len(scores)-1]=="":
+            #         scores.pop(len(scores)-1)
+            #         break
             scores.extend(file.readlines())
         return scores
 
@@ -31,10 +29,8 @@ class Player:
         :return: list of Player objects sorted by score.
         """
 
-        '''
-        scores.sort(key=operator.itemgetter(0))
-        scores.sort(key=operator.itemgetter(-2), reverse=True)
-        '''
+        # scores.sort(key=operator.itemgetter(0))
+        # scores.sort(key=operator.itemgetter(-2), reverse=True)
         scores.sort(key=lambda p: (-ord(p[-2]), p[0]))
         return scores
 
